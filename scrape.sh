242316e5fb2501669f4e3d659dcca96ddcd60c85
Alex Lavallee <73203142+lavalleeale@users.noreply.github.com>
2024-11-11 17:07:21 -0500

    update scraped data

--- scrape.sh
+++ scrape.sh
@@ -3,7 +3,9 @@
 # Example: scrape.sh ~/Documents/Programming/quacs/scrapers ~/Documents/Programming/racs
 set -e
 pushd $1
-python sis_scraper/main.py 202405 202409 202501
+python sis_scraper/main.py 202409
+python sis_scraper/main.py 202405
+python sis_scraper/main.py 202501
 jq -s '{"202405":.[0],"202409":.[1],"202501":.[2]}' -c data/202405/catalog_sis.json data/202409/catalog_sis.json data/202501/catalog_sis.json >$2/app/catalog_sis.json
 jq -s '{"202405":.[0], "202409":.[1],"202501":.[2]}' -c data/202405/courses.json data/202409/courses.json data/202501/courses.json >$2/app/courses.json
 popd
